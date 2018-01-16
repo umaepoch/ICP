6ea4fee524c9440d3791a1bc1baa8fbc86eb342f
Uma <umag@epochconsulting.in>
2018-01-16 11:11:25 +0530

    MReq Report

--- icp/icp/report/mreq_deliverystatus_report/mreq_deliverystatus_report.py
+++ icp/icp/report/mreq_deliverystatus_report/mreq_deliverystatus_report.py
@@ -64,12 +64,12 @@ def get_conditions(filters):
 def get_mr_details(filters):
         conditions = get_conditions(filters)
 	
-        return frappe.db.sql("""select pi.material_request as material_request, mr.modified_by, mr.requested_by, mr.status, mr.docstatus, pi.parent as purchase_order, pi.item_code as item_code, pi.description as description, pi.expected_delivery_date as expected_delivery_date, pi.revised_delivery_date as revised_delivery_date, mri.qty as req_qty, pi.qty as pi_qty, pi.received_qty as received_qty, (pi.received_qty/pi.qty*100) as per_received from `tabPurchase Order Item` pi, `tabMaterial Request` mr, `tabMaterial Request Item` mri where mr.name = mri.parent and pi.material_request = mr.name and mr.docstatus in ("0", "1") %s order by pi.material_request""" % conditions, as_dict=1)
+        return frappe.db.sql("""select pi.material_request as material_request, mr.modified_by, mr.requested_by, mr.status, mr.docstatus, pi.parent as purchase_order, pi.item_code as item_code, pi.description as description, pi.expected_delivery_date as expected_delivery_date, pi.revised_delivery_date as revised_delivery_date, mri.qty as req_qty, pi.qty as pi_qty, pi.received_qty as received_qty, (pi.received_qty/pi.qty*100) as per_received from `tabPurchase Order Item` pi, `tabMaterial Request` mr, `tabMaterial Request Item` mri where mr.name = mri.parent and pi.material_request = mr.name and mr.docstatus = "1" %s order by pi.material_request""" % conditions, as_dict=1)
 
 def get_mr_wo_po(filters):
         conditions = get_conditions(filters)
  
-	return frappe.db.sql("""select mr.name as material_request, mr.modified_by, mr.requested_by, mr.status, mr.docstatus, "" as purchase_order, mri.item_code as item_code, mri.description as description, "" as expected_delivery_date, "" as revised_delivery_date, mri.qty as req_qty, 0 as pi_qty, 0 as received_qty, 0 as per_received from `tabMaterial Request` mr, `tabMaterial Request Item` mri where mr.name = mri.parent and mr.docstatus in ("0", "1") %s and not exists (
+	return frappe.db.sql("""select mr.name as material_request, mr.modified_by, mr.requested_by, mr.status, mr.docstatus, "" as purchase_order, mri.item_code as item_code, mri.description as description, "" as expected_delivery_date, "" as revised_delivery_date, mri.qty as req_qty, 0 as pi_qty, 0 as received_qty, 0 as per_received from `tabMaterial Request` mr, `tabMaterial Request Item` mri where mr.name = mri.parent and mr.docstatus = "1" %s and not exists (
                 select 1 from `tabPurchase Order Item` pi where pi.material_request = mr.name) order by mr.name""" % conditions, as_dict=1)
 
 def get_item_map(filters):
