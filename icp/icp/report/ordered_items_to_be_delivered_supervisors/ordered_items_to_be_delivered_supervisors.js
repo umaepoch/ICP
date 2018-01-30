687269793324b411dc03b137f56aa8de3695c1b3
root <root@ip-172-31-23-141.ap-southeast-1.compute.internal>
2018-01-30 06:29:42 +0000

    Changes

---
+++ icp/icp/report/ordered_items_to_be_delivered_supervisors/ordered_items_to_be_delivered_supervisors.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Epoch and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Ordered Items to be Delivered-Supervisors"] = {
+	"filters": [
+
+	]
+}
