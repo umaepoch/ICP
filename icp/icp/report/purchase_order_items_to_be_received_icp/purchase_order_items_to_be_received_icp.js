c491b3e885f0da154d7a85857c85c56d9a3abf41
Uma <umag@epochconsulting.in>
2018-06-19 09:47:10 +0000

    PO Items to be received

---
+++ icp/icp/report/purchase_order_items_to_be_received_icp/purchase_order_items_to_be_received_icp.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Epoch and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Purchase Order Items To Be Received ICP"] = {
+	"filters": [
+
+	]
+}
